20aea964fec3288da47d3a22e7493b98b6ce0bb9
Isaac-DeFrain <isaacdefrain@gmail.com>
2026-05-08 21:49:00 -0700

    feat(docker): add init script to scrape and upload all opinions

---
+++ scripts/docker/run-init.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+# Source Docker environment variables dumped by cron-entrypoint.sh
+set -a
+. /etc/environment
+set +a
+
+cd /app
+npm run scrape-opinions -- --all && npm run upload-opinions
